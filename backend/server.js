52ca3b575c02f4fa7e0de7ef453f21a962da858a
Nisha <bishtnisha087@gmail.com>
2024-11-04 14:00:47 +0530

    created  GET endpoint for getting  all records from Listing model and sent the response to client side

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,41 @@ const express=require('express')
 const dotenv = require('dotenv');
 const Listing=require('./models/listing')
 const app=express();
+const cors=require('cors')
 // const initDB=require('./init/index')
 // initDB();
 dotenv.config();
 connectToDb();
+app.use(cors())
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/allListing",async(req,res)=>{
+    try{
+        const allListingData=await Listing.find()
+        if(allListingData.length>0)
+        {
+            res.send({
+                result:1,
+                result_value:allListingData
+            })
+
+        }
+        else{
+            res.send({
+                result:0,
+                error_value:"can not get the data"
+            })
+        }
+    }
+    catch(error){
+        res.send({
+            result:0,
+            error_value:error
+        })
+
+    }
+})
