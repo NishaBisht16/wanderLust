f3d13d66c9ad223560b88d17c6e86167cfa8abe4
Nisha <bishtnisha087@gmail.com>
2024-11-06 12:20:26 +0530

    created a get endpoint for displying the individual data in ui and sent the response to client side

--- backend/server.js
+++ backend/server.js
@@ -41,3 +41,33 @@ app.get("/allListing",async(req,res)=>{
 
     }
 })
+
+app.get('/listing/:id',async(req,res)=>{
+    const {id}=req.params;
+    try{
+        if(Listing.length>0)
+        {
+        const data=await Listing.findById({_id:id})
+        res.send({
+            result:1,
+            result_value:data
+        })
+        }
+    else{
+        res.send({
+            result:0,
+            error_value:'Data is not avaliable'
+        })
+    }
+    }
+catch(error)
+{
+    res.send({
+        result:0,
+        error_value:error
+    })
+    
+}
+
+
+})
