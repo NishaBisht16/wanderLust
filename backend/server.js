9c02101dbe12f67da2aed2453ec6a72eab2810d2
Nisha <bishtnisha087@gmail.com>
2024-11-07 11:47:10 +0530

    Created the update route and impliment the update functionality in Listing Model

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express=require('express')
 const dotenv = require('dotenv');
 const Listing=require('./models/listing')
 const app=express();
-const cors=require('cors')
+const cors=require('cors');
+const { default: mongoose } = require('mongoose');
 // const initDB=require('./init/index')
 // initDB();
 dotenv.config();
@@ -102,7 +103,58 @@ app.post('/create',async(req,res)=>{
 
 
 app.get('/Edit/:id',async(req,res)=>{
-    const {id}=req.params
-    const data=await Listing.findById({_id:id})
-    res.send(data)
+    const {id}=req.params;
+        if(Listing.length>0)
+        {
+        const data=await Listing.findById({_id:id})
+        res.send({
+            result:1,
+            result_value:{data}
+        })
+        }
+    else{
+        res.send({
+            result:0,
+            error_value:'Data is not avaliable'
+        })
+    }
+     
+})
+
+app.post('/update/:id',async(req,res)=>{
+    try{
+        const {id}=req.params
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ error: "Invalid ID format" });
+        }
+
+        const {title,price,location,country}=req.body
+        const updateData = await Listing.findByIdAndUpdate(
+              id,
+            { title:title, price:price, location:location, country:country },
+            { new: true }
+        );
+
+        if(updateData)
+        {
+            res.send({
+                result:1,
+                message:"Data updated successfully"
+            })
+        }
+        else{
+            res.send({
+                result:0,
+                message:"Data did not update"
+            })
+        }
+    }
+    catch(error)
+    {
+        res.send({
+            result:1,
+            message:error
+        })
+
+    }
 })
